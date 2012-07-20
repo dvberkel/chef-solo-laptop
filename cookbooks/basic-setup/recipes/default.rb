70696f642790e58c9c1677e7013ee66e1c8f50cd
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-20 09:23:28 +0200

    oCaml added
    oCaml is added to the programming languages.

--- cookbooks/basic-setup/recipes/default.rb
+++ cookbooks/basic-setup/recipes/default.rb
@@ -6,7 +6,7 @@
 
 # all package to install
 vcss = %w{bzr mercurial subversion}
-languages = %w{ghc6 swi-prolog}
+languages = %w{ghc6 swi-prolog ocaml}
 java = %w{openjdk-6-jdk openjdk-6-doc openjdk-6-source openjdk-7-jdk openjdk-7-doc openjdk-7-source}
 packages = [ vcss, languages, java ]
 
