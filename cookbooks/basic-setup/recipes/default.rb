c849e222632e0ebc4be2407537de2feb20fe5859
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-20 10:22:21 +0200

    Mathematics
    Some mathematics is added to the cookbook.

--- cookbooks/basic-setup/recipes/default.rb
+++ cookbooks/basic-setup/recipes/default.rb
@@ -6,9 +6,10 @@
 
 # all package to install
 vcss = %w{bzr mercurial subversion}
+mathematics = %w{geogebra genius coqide}
 languages = %w{ghc6 swi-prolog ocaml erlang}
 java = %w{openjdk-6-jdk openjdk-6-doc openjdk-6-source openjdk-7-jdk openjdk-7-doc openjdk-7-source}
-packages = [ vcss, languages, java ]
+packages = [ vcss, mathematics, languages, java ]
 
 packages.flatten.each do |a_package|
   package a_package
