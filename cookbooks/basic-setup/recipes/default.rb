001fb7f2bc7d3c6d90bd281340bbfb41f96db281
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-07-24 10:47:18 +0200

    Scheme added
    Scheme is added to the list of programming languages.

--- cookbooks/basic-setup/recipes/default.rb
+++ cookbooks/basic-setup/recipes/default.rb
@@ -7,7 +7,7 @@
 # all package to install
 vcss = %w{bzr mercurial subversion}
 mathematics = %w{texlive-full geogebra genius coqide}
-languages = %w{ghc6 swi-prolog ocaml erlang}
+languages = %w{ghc6 swi-prolog ocaml erlang mit-scheme}
 java = %w{openjdk-6-jdk openjdk-6-doc openjdk-6-source openjdk-7-jdk openjdk-7-doc openjdk-7-source}
 packages = [ vcss, mathematics, languages, java ]
 
@@ -21,3 +21,6 @@ recipes = %w{rvm nodejs nodejs::npm arduino}
 recipes.each do |a_recipe|
   include_recipe a_recipe
 end
+
+# all files to create
+
