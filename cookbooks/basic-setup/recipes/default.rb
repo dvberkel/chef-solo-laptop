1171ff1820ff8f912ee023d446785bed79ae419b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2013-02-27 19:33:29 +0100

    [REFACTOR] use haskell-platform

--- cookbooks/basic-setup/recipes/default.rb
+++ cookbooks/basic-setup/recipes/default.rb
@@ -7,7 +7,7 @@
 # all package to install
 vcss = %w{bzr mercurial subversion}
 mathematics = %w{texlive-full geogebra genius coqide}
-languages = %w{ghc6 swi-prolog ocaml erlang mit-scheme}
+languages = %w{haskell-platform swi-prolog ocaml erlang mit-scheme}
 java = %w{openjdk-6-jdk openjdk-6-doc openjdk-6-source openjdk-7-jdk openjdk-7-doc openjdk-7-source maven2}
 databases = %w{redis-server}
 packages = [ vcss, mathematics, languages, java, databases]
@@ -21,7 +21,6 @@ utils = %w{rvm phantomjs}
 recent_languages = %w{nodejs nodejs::npm leiningen go}
 recent_databases = %w{couchdb mysql::server}
 electronics = %w{arduino}
-gurps = %w{gcs}
 recipes = [ utils, recent_languages, recent_databases, electronics, gurps ]
 
 recipes.flatten.each do |a_recipe|
